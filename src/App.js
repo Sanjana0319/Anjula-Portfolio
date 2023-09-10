917fe43eec6f571e9b77d745f494f33558d5e3af
Anjula Shanaka <anjulashanaka@gmail.com>
2023-09-10 11:34:34 +0530

    Update the directory structure (#8)
    * update
    
    * Update the directory structure
    
    ---------
    
    Co-authored-by: sanjana0319 <asanjana0823@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Home from "./home";
-import SideBar from "./sidebar";
+import Home from "./components/home";
+import SideBar from "./components/sidebar";
 
 function App() {
   return (
